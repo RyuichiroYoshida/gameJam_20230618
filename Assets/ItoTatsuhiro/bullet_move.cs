fbba9969f9ba8a864dcedd55ef0ad093bbbd18c0
ItoTatsuhiro <ito.3229.wo@gmail.com>
2023-06-18 17:06:17 +0900

    ・プレイヤーの動き更新 ・弾の動きの追加

---
+++ Assets/ItoTatsuhiro/bullet_move.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class bullet_move : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.Translate(0.05f, 0, 0);        //’e‚ÌˆÚ“®
+
+        if (transform.position.x > 12)
+        {
+            Destroy(gameObject);    //‰æ–ÊŠO‚Éo‚½’e‚ğÁ‚·
+        }
+    }
+}
